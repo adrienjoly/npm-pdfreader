77e52f9791240b7e126674e91f26f888a26acf46
Adrien Joly <adrien@whyd.com>
2015-03-17 17:06:14 +0100

    v0.0.4: exports more libs

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 exports.PdfReader = require("./PdfReader");
 exports.Rule = require("./Rule");
+exports.LOG = require("./lib/LOG.js");
+exports.parseTable = require("./lib/parseTable.js");
+exports.parseColumns = require("./lib/parseColumns.js");
