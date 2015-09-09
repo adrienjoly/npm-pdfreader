d4779023cc1d195dd714441c941d4087ac08e2e9
Adrien Joly <adrien@whyd.com>
2015-09-09 14:03:56 +0200

    [experiment] added SequentialParser as an alternative to Rules => v0.1.4

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ exports.Rule = require("./Rule");
 exports.LOG = require("./lib/LOG.js");
 exports.parseTable = require("./lib/parseTable.js");
 exports.parseColumns = require("./lib/parseColumns.js");
+exports.SequentialParser = require("./lib/SequentialParser.js"); // experimental
 exports.TableParser = require("./lib/TableParser.js");
 exports.ColumnsParser = require("./lib/ColumnsParser.js");
