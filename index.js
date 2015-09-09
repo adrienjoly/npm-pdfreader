0dab67bc86a74a1910ada8c6f4b2b55171d5b063
Adrien Joly <adrien@whyd.com>
2015-09-09 10:07:52 +0200

    added agnostic ColumnsParser and TableParser functions

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ exports.Rule = require("./Rule");
 exports.LOG = require("./lib/LOG.js");
 exports.parseTable = require("./lib/parseTable.js");
 exports.parseColumns = require("./lib/parseColumns.js");
+exports.TableParser = require("./lib/TableParser.js");
+exports.ColumnsParser = require("./lib/ColumnsParser.js");
