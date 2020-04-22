cf510b6eda30edc056712601ebc2cb1015a728cd
nbuddiga-xybion <56626714+nbuddiga-xybion@users.noreply.github.com>
2020-04-22 15:32:21 +0530

    Update PdfReader.js

--- PdfReader.js
+++ PdfReader.js
@@ -15,55 +15,59 @@ var LOG = require("./lib/LOG.js");
 var PFParser = require("pdf2json/pdfparser"); // doc: https://github.com/modesty/pdf2json
 
 function forEachItem(pdf, handler) {
-  var pageNumber = 0;
-  for (var p in pdf.formImage.Pages) {
-    var page = pdf.formImage.Pages[p];
-    var number = ++pageNumber;
-    handler(null, {
-      page: number,
-      width: pdf.formImage.Width,
-      height: pdf.formImage.Pages[number - 1].Height
-    });
-    for (var t in page.Texts) {
-      var item = page.Texts[t];
-      item.text = decodeURIComponent(item.R[0].T);
-      handler(null, item);
+    var pageNumber = 0;
+    for (var p in pdf.formImage.Pages) {
+        if (p !== "equals") {
+            var page = pdf.formImage.Pages[p];
+            var number = ++pageNumber;
+            handler(null, {
+                page: number,
+                width: pdf.formImage.Width,
+                height: pdf.formImage.Pages[number - 1].Height
+            });
+            for (var t in page.Texts) {
+                if (t !== "equals") {
+                    var item = page.Texts[t];
+                    item.text = decodeURIComponent(item.R[0].T);
+                    handler(null, item);
+                }
+            }
+        }
     }
-  }
-  handler();
+    handler();
 }
 
 function PdfReader(options) {
-  LOG("PdfReader"); // only displayed if LOG.js was first loaded with `true` as init parameter
-  this.options = options || {};
+    LOG("PdfReader"); // only displayed if LOG.js was first loaded with `true` as init parameter
+    this.options = options || {};
 }
 
 /**
  * parseFileItems: calls itemHandler(error, item) on each item parsed from the pdf file
  **/
 PdfReader.prototype.parseFileItems = function(pdfFilePath, itemHandler) {
-  itemHandler(null, { file: { path: pdfFilePath } });
-  var pdfParser = new PFParser();
-  pdfParser.on("pdfParser_dataError", itemHandler);
-  pdfParser.on("pdfParser_dataReady", function(pdfData) {
-    forEachItem(pdfData, itemHandler);
-  });
-  var verbosity = this.options.debug ? 1 : 0;
-  pdfParser.loadPDF(pdfFilePath, verbosity);
+    itemHandler(null, { file: { path: pdfFilePath } });
+    var pdfParser = new PFParser();
+    pdfParser.on("pdfParser_dataError", itemHandler);
+    pdfParser.on("pdfParser_dataReady", function(pdfData) {
+        forEachItem(pdfData, itemHandler);
+    });
+    var verbosity = this.options.debug ? 1 : 0;
+    pdfParser.loadPDF(pdfFilePath, verbosity);
 };
 
 /**
  * parseBuffer: calls itemHandler(error, item) on each item parsed from the pdf file received as a buffer
  */
 PdfReader.prototype.parseBuffer = function(pdfBuffer, itemHandler) {
-  itemHandler(null, { file: { buffer: pdfBuffer } });
-  var pdfParser = new PFParser();
-  pdfParser.on("pdfParser_dataError", itemHandler);
-  pdfParser.on("pdfParser_dataReady", function(pdfData) {
-    forEachItem(pdfData, itemHandler);
-  });
-  var verbosity = this.options.debug ? 1 : 0;
-  pdfParser.parseBuffer(pdfBuffer, verbosity);
+    itemHandler(null, { file: { buffer: pdfBuffer } });
+    var pdfParser = new PFParser();
+    pdfParser.on("pdfParser_dataError", itemHandler);
+    pdfParser.on("pdfParser_dataReady", function(pdfData) {
+        forEachItem(pdfData, itemHandler);
+    });
+    var verbosity = this.options.debug ? 1 : 0;
+    pdfParser.parseBuffer(pdfBuffer, verbosity);
 };
 
 module.exports = PdfReader;
