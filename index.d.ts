8d76500b863d7a732e58378383ddd2d68c1a3359
Nozomi Yasuda <nozomi.yasuda.4649@gmail.com>
2024-08-04 09:18:27 -0400

    fix(TS): Add type definitions for Item and TableParser (#155)
    * Update index.d.ts
    
    Added type definitions for TableParser class
    
    * fixed formatting
    
    ---------
    
    Co-authored-by: Adrien Joly <531781+adrienjoly@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -15,7 +15,35 @@ export type DataEntry = {
 export type ItemHandler = (err: Error, data: DataEntry) => void;
 
 export declare class PdfReader {
-  constructor(opts: InitOptions | null);
+  constructor(opts?: InitOptions | null);
   parseFileItems(pdfFilePath: string, itemHandler: ItemHandler): void;
   parseBuffer(buffer: Buffer, itemHandler: ItemHandler): void;
+}
+
+export type Item = {
+  x: number;
+  y: number;
+  sw: number;
+  w: number;
+  A: string;
+  clr: number;
+  R: {
+    T: string;
+    S: number;
+    TS: any[];
+  }[];
+  text: string;
+};
+
+export class TableParser {
+  private rows: { [key: string]: Item[] };
+  constructor();
+  processItem(item: Item, col: number): void;
+  processHeadingItem(item: Item, col: number): void;
+  getRows(): Item[][];
+  renderRows(): string;
+  /** row-> column-> items_collisionning_in_column-> item:Item */
+  getMatrix(): Item[][][];
+  getCleanMatrix(options?: { collisionSeparator: string }): string[][];
+  renderMatrix(): string;
 }
